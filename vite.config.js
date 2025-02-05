c877e041311811d784089399affda8d90548ed19
Rajnish Puri <rajnishpuri15@gmail.com>
2025-02-05 19:46:13 +0530

    ui

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // https://vitejs.dev/config/
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Razz-Portfolio/', // Set this to your repository name
+  base: '/Razz-Portfolio/',  // Make sure this matches your repo name
+  plugins: [react()],
 });
